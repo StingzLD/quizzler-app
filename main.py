2b59d054e307b7265f6aa724d3a28dee81544bef
Corey Hastings <stingzld@gmail.com>
2023-08-03 17:46:05 -0500

    Created UI

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from classes import Question, QuizBrain
 from data import question_data
+from ui import QuizInterface
 
 question_bank = []
 for q in question_data:
@@ -7,11 +8,12 @@ for q in question_data:
     question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
-still_has_questions = quiz.still_has_questions()
+quiz_ui = QuizInterface()
 
-while quiz.still_has_questions():
-    quiz.next_question()
-    quiz.still_has_questions()
+still_has_questions = quiz.still_has_questions()
+# while quiz.still_has_questions():
+#     quiz.next_question()
+#     quiz.still_has_questions()
 
 print("You have completed the quiz")
 print(f"Your final score is: {quiz.score}/{quiz.question_number}")
